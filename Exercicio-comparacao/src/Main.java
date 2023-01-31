252d716444fa11803af7564a123121924520c4c0
Russor <107722401+russorlima@users.noreply.github.com>
2023-01-31 20:13:07 -0300

    exercicios Java

---
+++ Exercicio-comparacao/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        if (!b2) {
+            System.out.println("Verdade");
+        else {
+            System.out.println("Mentira");
+            System.out.println("Mentira");
+        }
+    }
+}
